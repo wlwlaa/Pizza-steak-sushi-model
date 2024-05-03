1d67732b7d1cd6985ddfd99fad38d50c2da7d000
Andrey <112896514+wlwlaa@users.noreply.github.com>
2024-05-04 00:26:59 +0300

    Update utils.py

--- pizza-steak-sushi-model/utils.py
+++ pizza-steak-sushi-model/utils.py
@@ -3,6 +3,10 @@
 """
 import torch
 from pathlib import Path
+from typing import List, Tuple
+
+from PIL import Image
+from torchvision.transforms import v2 as transforms
 
 def save_model(
     model: torch.nn.Module,
@@ -31,7 +35,81 @@ def save_model(
 
   # Сохраним state_dict модели
   print(f'[INFO] Сохранение модели в {target_dir_path}')
+      
   torch.save(
       obj=model.state_dict(),
       f=model_save_path
+  )
+
+
+# 1. Получим данные
+def predAndPlotImage(
+    model: torch.nn.Module,
+    image_path: str,
+    class_names: List[str],
+    image_size: Tuple[int, int] = (224, 224),
+    transform: torchvision.transforms = None,
+    device: torch.device = device
+  ) -> None:
+  # 2. Откроем изображение с помощью PIL
+  img = Image.open(image_path)
+
+  # 3. Создадим преобразования, если они не заданы
+  if not transform:
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToImage(),
+        transforms.ToDtype(torch.float32, scale=True),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        )
+    ])
+
+  ### Классификация ###
+  # 4. Убедимся, что модель на нужном девайсе
+  model.to(device)
+
+  # 5. Включим режим получения результатов
+  model.eval()
+  with torch.inference_mode():
+    # 6. Преобразуем изображение
+    transformed_img = transform(img).unsqueeze(dim=0).to(device) # [batch_size, color_channels, height, width]
+
+    # 7. Получим вероятности
+    preds = model(transformed_img).softmax(dim=1).squeeze(dim=0)
+
+  # 8. Получим метку
+  label = preds.argmax(dim=0).item()
+
+  # 9. Выведем результат
+  plt.figure()
+  plt.imshow(img)
+  plt.axis(False)
+  plt.title(f'Это {class_names[label]} с вероятностью {(preds[label] * 100):.2f}%');
+
+
+def predCustomImage(
+    model: nn.Module,
+    url: str,
+    class_names: List[str],
+    data_path: str
+  ) -> None:
+  data_path = Path(data_path)
+
+  if not data_path.is_dir():
+    data_path.mkdir(parents=True)
+    print(f'[INFO] Создаем папку {data_path}')
+
+  custom_path = data_path / url.split('/')[-1]
+  print(f'[INFO] Загрузим {custom_path}...')
+  responce = requests.get(url)
+
+  with open(custom_path, 'wb') as f:
+    f.write(responce.content)
+
+  predAndPlotImage(
+      model=model,
+      image_path=custom_path,
+      class_names=class_names
   )
